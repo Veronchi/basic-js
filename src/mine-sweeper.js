57a29a8ee61e052da98e2b3578afce568f4e46a5
Veronika <zhigunova1995@gmail.com>
2022-10-17 17:54:49 +0300

    feat: add solution for task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let resultArr = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    let innerArr = [];
+    for (let k = 0; k < matrix[i].length; k++) {
+      let numOfMines = 0;
+
+      if (matrix[i][k - 1]) numOfMines += 1;
+      if (matrix[i][k + 1]) numOfMines += 1;
+      if (i > 0) {
+        if (matrix[i - 1][k - 1]) numOfMines += 1;
+        if (matrix[i - 1][k]) numOfMines += 1;
+        if (matrix[i - 1][k + 1]) numOfMines += 1;
+      }
+      if (i < matrix.length - 1) {
+        if (matrix[i + 1][k - 1]) numOfMines += 1;
+        if (matrix[i + 1][k]) numOfMines += 1;
+        if (matrix[i + 1][k + 1]) numOfMines += 1;
+      }
+
+      innerArr[k] = numOfMines;
+    }
+
+    resultArr.push(innerArr);
+  }
+
+  return resultArr;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
